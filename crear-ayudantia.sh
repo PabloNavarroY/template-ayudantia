d512060dc1584b12c7ac11ec772903d5dc2f4bc7
Pourtz <pany182001@gmail.com>
2024-03-14 12:45:10 -0300

    Initial commit

---
+++ crear-ayudantia.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env sh
+
+folder="ayudantia$1"
+
+mkdir -p "$folder"
+
+if [ -f "$folder/enunciado.tex" ]; then
+    echo "$folder/enunciado.tex ya existe"
+else
+    cp "template/enunciado.tex" "$folder"
+    sed -i 's/NUMM/'"$1"'/' "$folder/enunciado.tex"
+fi
+
+if [ -f "$folder/solucion.tex" ]; then
+    echo "$folder/solucion.tex ya existe"
+else
+    cp "template/solucion.tex" "$folder"
+    sed -i 's/NUMM/'"$1"'/' "$folder/solucion.tex"
+fi
+
+cp "template/.latexmkrc" "$folder"
---
+++ template/.latexmkrc
@@ -0,0 +1,4 @@
+@default_files = ('enunciado.tex' 'solucion.tex');
+
+$pdf_mode = 1;
+$postscript_mode = $dvi_mode = 0;
